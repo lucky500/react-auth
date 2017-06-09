69829ce727c8036d890808b9b74ebfe0118aa1d6
Luciano Oliveira <luciano101@msn.com>
2017-06-09 18:59:09 -0400

    project completed

---
+++ README.md
@@ -0,0 +1,5 @@
+a rewrite of a template using React.js
+
+this project used Eduonix - Products in React https://www.udemy.com/projects-in-reactjs-the-complete-react-learning-course/
+
+as well, create-react-app
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+      	<h1>Welcome</h1>
+      	<p>Please login to view dashboard</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
